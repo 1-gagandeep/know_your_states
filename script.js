78ffe24451fe5e08d9d0fc3aee916f1450c9a41a
1-gagandeep <gagandeepkumar294@gmail.com>
2025-02-10 20:33:55 +0530

    governor data updated

--- script.js
+++ script.js
@@ -110,3 +110,14 @@ modeBtn.addEventListener("click", () => {
 
     console.log(currMode);
 });
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const hamburger = document.querySelector(".hamburger");
+//     const navbarMenu = document.querySelector(".navbar-menu");
+
+//     hamburger.addEventListener("click", function () {
+//         navbarMenu.classList.toggle("active");
+//     });
+// });
+
